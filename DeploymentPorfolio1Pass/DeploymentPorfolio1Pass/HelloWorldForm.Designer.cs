cb8ff6d648c025ac0d806487ca55a89010ed6eba
Linh Dan Nguyen <89968584+msjuliee@users.noreply.github.com>
2024-05-16 23:53:55 +0700

    Add files via upload

---
+++ DeploymentPorfolio1Pass/DeploymentPorfolio1Pass/HelloWorldForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DeploymentPorfolio1Pass
+{
+    partial class HelloWorldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.myButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(162, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DEPLOYMENT PORFOLIO TASK 1.1 PASS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(301, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HELLO WORLD!!";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(286, 188);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(339, 22);
+            this.nameBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter your name";
+            // 
+            // myButton
+            // 
+            this.myButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.myButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myButton.ForeColor = System.Drawing.Color.White;
+            this.myButton.Location = new System.Drawing.Point(333, 246);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(108, 31);
+            this.myButton.TabIndex = 4;
+            this.myButton.Text = "Join Us";
+            this.myButton.UseVisualStyleBackColor = false;
+            this.myButton.Click += new System.EventHandler(this.myButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(253)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Task 1.1 Pass";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button myButton;
+    }
+}
+
---
+++ DeploymentPorfolio1Pass/DeploymentPorfolio1Pass/HelloWorldForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeploymentPorfolio1Pass
+{
+    public partial class HelloWorldForm : Form
+    {
+        public HelloWorldForm()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            String name = nameBox.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Please enter your name.");
+            }
+            else
+            {
+                MessageBox.Show(" Hello " + name + "! Welcome to SWE40006 - Software Deployment and Evolution!");
+            }
+        }
+    }
+}
